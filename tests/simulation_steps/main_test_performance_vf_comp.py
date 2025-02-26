f1e5d4fedbf6e748e677a41f7e8dcf40505bb650
Eliewiii <elie.medioniwiii@gmail.com>
2025-02-26 11:11:04 +0200

    ready for benchmark

---
+++ tests/simulation_steps/main_test_performance_vf_comp.py
@@ -0,0 +1,89 @@
+
+
+import os
+
+from src.bua.urban_canopy import UrbanCanopy
+from src.bua.simulation_steps import *
+from src.bua.config.config_default_values_user_parameters import *
+
+# Inputs to be used
+test_file_dir = os.path.dirname(os.path.abspath(__file__))
+test_folder_dir = os.path.dirname(test_file_dir)
+test_data_dir = os.path.join(test_folder_dir, "test_data")
+path_test_building_hbjson_0 = os.path.join(test_data_dir, "test_hbjsons", "Building_sample_0.hbjson")
+path_test_building_hbjson_1 = os.path.join(test_data_dir, "test_hbjsons", "Building_sample_1.hbjson")
+path_test_lb_polyface3d_context = os.path.join(test_data_dir, "test_lb_polyface3d_context.json")
+# print("")
+# print(f"path_test_building_hbjson_0: {path_test_building_hbjson_0}")
+# print(f"path_test_building_hbjson_1: {path_test_building_hbjson_1}")
+# print(f"path_test_lb_polyface3d_context: {path_test_lb_polyface3d_context}")
+
+
+
+def init_urban_canopy_with_all_buildingmodels() -> UrbanCanopy:
+    """
+    Generate an UrbanCanopy object with 2 BuildingModeled and BuildingBasic objects
+    """
+    # Clear simulation temp folder
+    SimulationCommonMethods.clear_simulation_temp_folder()
+    # Create simulation folder
+    SimulationCommonMethods.make_simulation_folder(path_simulation_folder=default_path_simulation_folder)
+    # Create an UrabanCanopy object
+    urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object(
+        path_simulation_folder=default_path_simulation_folder)
+    # Load HBJSONs
+    SimulationLoadBuildingOrGeometry.add_buildings_from_hbjson_to_urban_canopy(
+        urban_canopy_object=urban_canopy_object,
+        path_folder_hbjson=os.path.join(test_data_dir, "test_hbjsons"),
+        path_file_hbjson=None,
+        are_buildings_targets=True,
+        keep_context_from_hbjson=False)
+
+    return urban_canopy_object
+
+def init_urban_canopy_with_two_buildingmodels() -> UrbanCanopy:
+    """
+    Generate an UrbanCanopy object with 2 BuildingModeled and BuildingBasic objects
+    """
+    # Clear simulation temp folder
+    SimulationCommonMethods.clear_simulation_temp_folder()
+    # Create simulation folder
+    SimulationCommonMethods.make_simulation_folder(path_simulation_folder=default_path_simulation_folder)
+    # Create an UrabanCanopy object
+    urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object(
+        path_simulation_folder=default_path_simulation_folder)
+    # Load HBJSONs
+    SimulationLoadBuildingOrGeometry.add_buildings_from_hbjson_to_urban_canopy(
+        urban_canopy_object=urban_canopy_object,
+        path_folder_hbjson=None,
+        path_file_hbjson=path_test_building_hbjson_0,
+        are_buildings_targets=True,
+        keep_context_from_hbjson=False)
+    SimulationLoadBuildingOrGeometry.add_buildings_from_hbjson_to_urban_canopy(
+        urban_canopy_object=urban_canopy_object,
+        path_folder_hbjson=None,
+        path_file_hbjson=path_test_building_hbjson_1,
+        are_buildings_targets=True,
+        keep_context_from_hbjson=False)
+
+    return urban_canopy_object
+
+
+def run_vf_comp_from_subprocess(urban_canopy_obj,include_windows=True):
+    """
+
+    """
+    SimulationLWR.generate_radiative_surface_manager_for_lwr_computation(urban_canopy_obj,
+                                                                         overwrite=True,
+                                                                         include_windows=True)
+
+    SimulationLWR.perform_lwr_vf_computation(urban_canopy_obj, overwrite=True, save_to_pkl= True)
+
+
+if __name__ == "__main__":
+    # include_windows = True
+    include_windows = False
+    urban_canopy_obj = init_urban_canopy_with_all_buildingmodels()
+    # urban_canopy_obj = init_urban_canopy_with_two_buildingmodels()
+
+    run_vf_comp_from_subprocess(urban_canopy_obj)
